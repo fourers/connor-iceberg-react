104e7721a7fd9e5b5f818904d695d130a6147c31
fourers <fourersxd@gmail.com>
2023-07-27 21:51:02 +1000

    Switch to basic bounds calculation

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { MapContainer, ImageOverlay } from 'react-leaflet';
-import { generateBounds } from '../util';
 import './App.css';
 import Atlas from './Atlas';
 import EventHandlers from './EventHandlers';
+
+const generateBounds = (width, height) => {
+    const longitude = width / 500;
+    const latitude = height / 500;
+    return [
+        [-latitude, -longitude],
+        [latitude, longitude],
+    ];
+};
 
 export default function App() {
     const width = 640;
     const height = 1143;
     const bounds = generateBounds(width, height);
-    const maxBounds = generateBounds(width, height + 300);
+    const maxBounds = generateBounds(width + 100, height + 300);
+    const minZoom = 7.5;
+    const maxZoom = 10;
 
     return (
         <MapContainer
             center={[0, 0]}
-            zoom={8}
-            minZoom={8}
-            maxZoom={10}
+            zoom={minZoom}
+            minZoom={minZoom}
+            maxZoom={maxZoom}
             maxBounds={maxBounds}
             interactive
             zoomSnap={0.25}
